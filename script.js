e4778dd2b58e163b0ee63413ff4499a194318264
James Hughes <heezmagnif@gmail.com>
2017-08-02 12:53:31 -0400

    For loops

--- script.js
+++ script.js
@@ -323,3 +323,26 @@ var mike = {
 mike.calculateAge();
 console.log(mike);
 */
+
+///////////////////////////////////////////
+// Lecture: Loops
+
+for (var i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+/*
+0, true, print 0, update i to 1
+1, true, print 1, update i to 2
+.
+.
+.
+9, true, print 9, update i to 9
+10, FALSE, end loop!
+*/
+
+var names = ['John', 'Jane', 'Mary', 'Mark', 'Bob'];
+
+for (var i = 0; i < 5, i++) {
+    console.log(names[i]);
+}
